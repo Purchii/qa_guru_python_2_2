2b2a42ad2d5f0a1d4496f6034d2c3e99c58a8232
Purchii <oleg.chikhalin@gmail.com>
2022-08-31 17:09:14 +0300

    added failed (not working) test for homework #2

--- test_python_2_2.py
+++ test_python_2_2.py
@@ -9,3 +9,8 @@ import conftest
 def test_google_should_find_selene(open_browser_set_reso):
     browser.element('[name="q"]').should(be.blank).type('selene git').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_google_should_find_selene_not(open_browser_set_reso):
+    browser.element('[name="q"]').should(be.blank).type('git').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))

883c6209f5d4ddbcb2eab422d9b870ca93af1bd3
Oleg Chikhalin <oleg.chikhalin@gmail.com>
2022-09-04 22:43:41 +0300

    Update test_python_2_2.py
    fixed import for browser and test_google_should_find_selene_not test

--- test_python_2_2.py
+++ test_python_2_2.py
@@ -1,5 +1,6 @@
 import pytest
-from selene import browser, be, have
+from selene.support.shared import browser
+from selene import be, have
 import conftest
 
 
@@ -12,5 +13,5 @@ def test_google_should_find_selene(open_browser_set_reso):
 
 
 def test_google_should_find_selene_not(open_browser_set_reso):
-    browser.element('[name="q"]').should(be.blank).type('git').press_enter()
-    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+    browser.element('[name="q"]').should(be.blank).type('selene git').press_enter()
+    browser.element('[id="search"]').should(have.no.text('User-oriented Web UI browser tests in Python'))

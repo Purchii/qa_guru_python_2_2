5679c5de791f06b81865127b76f320ef2e0c849d
Oleg Chikhalin <oleg.chikhalin@gmail.com>
2022-09-05 19:18:24 +0300

    Update test_python_2_2.py
    added some interesting text to make second test hopefully pass finally

--- test_python_2_2.py
+++ test_python_2_2.py
@@ -13,5 +13,5 @@ def test_google_should_find_selene(open_browser_set_reso):
 
 
 def test_google_should_find_selene_not(open_browser_set_reso):
-    browser.element('[name="q"]').should(be.blank).type('selene git').press_enter()
-    browser.element('[id="search"]').should(have.no.text('User-oriented Web UI browser tests in Python'))
+    browser.element('[name="q"]').should(be.blank).type('djhgkjrhflkgdjhglfg;jld').press_enter()
+    browser.element('[id="search"]').should(have.no.text('djhgkjrhflkgdjhglfg;jld'))
